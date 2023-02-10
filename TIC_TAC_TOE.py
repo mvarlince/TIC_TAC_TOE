16f23428ac917414cfb4f1267d707c2b1e6aa2dd
Varlince Meran <varlincemeran@gmail.com>
2023-02-10 09:15:37 -0500

    cleaned up spacing

--- TIC_TAC_TOE.py
+++ TIC_TAC_TOE.py
@@ -12,26 +12,21 @@ def display_board(board):
    
 
 def player_input():
-    
     marker = ''
-    
     while marker != 'X' and marker != 'O':
         marker = input('Player 1: Choose X or O: ').upper()
-        
     if marker == 'X':
         return ('X','O')
     else:
         return ('O','X')   
 
+
 def place_marker(board, marker, position):
-    
     board[position] = marker
 
 
 def win_check(board, mark):
-       
     return (       
-        
     (board[7] == mark and board[8] == mark and board[9] == mark) or
     (board[4] == mark and board[5] == mark and board[6] == mark) or
     (board[1] == mark and board[2] == mark and board[3] == mark) or
@@ -42,10 +37,8 @@ def win_check(board, mark):
     (board[9] == mark and board[5] == mark and board[1] == mark))    
 
 
-
 def choose_first():
     flip = random.randint(0,1)
-    
     if flip == 0:
         return 'Player 1'
     else:
@@ -53,12 +46,10 @@ def choose_first():
 
 
 def space_check(board, position):
-    
     return board[position] == ' '
 
 
 def full_board_check(board):
-    
     for i in range(1,10):
         if space_check(board,i):
             return False
@@ -67,28 +58,21 @@ def full_board_check(board):
 
 
 def player_choice(board):
-    
     position = 0
     while position not in range(1,10) or not space_check(board,position):
         position = int(input('Choose a position: (1-9) '))
-    
     return position
 
 
 def replay():  
-    
     userplay = input('Do you want to play again? Enter yes or no ')
     return userplay == 'yes'
 
-
-
-        
+   
 print('Welcome to Tic Tac Toe!')
 
 while True:
-    
     # Play the game
-    
     ## SET EVERYTHING UP (BOARD, WHOS FIRST, CHOOSE MARKERS X 0)
     the_board = [' ']*10
     player1_marker, player2_marker = player_input()
@@ -97,14 +81,12 @@ while True:
     print(turn + ' will go first')
     
     play_game = input('Ready to play? y or n? ')
-                      
-                      
+                               
     if play_game == 'y': 
         game_on = True
     else:
         game_on = False
-                      
-                      
+                                
     ## Game Play
     while game_on:
         
@@ -149,7 +131,6 @@ while True:
                     game_on = False
                 else:
                     turn = 'Player 1'
-        
-                      
+                     
     if not replay():
         break
